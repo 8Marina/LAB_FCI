0036ada94d02c9c7116fa03c399b5ade92229b60
8Marina <72970864+8Marina@users.noreply.github.com>
2021-03-29 10:49:04 +0200

    Update tempo_risposta_3url.py

--- Lab1/tempo_risposta_3url.py
+++ Lab1/tempo_risposta_3url.py
@@ -1,1 +1,17 @@
+import requests
 
+siti = ['http://www.google.com',  'http://www.youtube.com', 'http://www.facebook.com']
+
+for url in siti:
+    print(' URL: ', url)
+    tempi = []
+
+    for j in range(10):
+        r = requests.get(url)
+        tempi.append(r.elapsed.microseconds / 1000)
+
+    print('Minimo tempo di risposta: ', min(tempi))
+    print('Massimo tempo di risposta: ', max(tempi))
+    print('Tempo di risposta medio: ', sum(tempi) / len(tempi))
+    m = max([m, max(times)])
+    plt.plot(times, label=url)
